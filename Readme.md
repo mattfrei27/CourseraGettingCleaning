8dd77e5f902e5c05c4333911b47c671f346e02fe
Matt Frei <mattfrei27@gmail.com>
2015-12-19 16:36:02 -0700

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+Getting and Cleaning Data: Course Project
+======================
+
+The run_analysis.R script downloads Samsung smartphone sensor data originally from the UCI machine learning repository. It has a variety of sensor measurements on subjects who performed activities such as climbing stairs and walking while the smartphone sensors recorded the movements. 
+
+run_analysis.R also performs the tasks required for the Coursera Getting and Cleaning Data course:
+
+*Merges the training and the test sets to create one data set.
+*Extracts only the measurements on the mean and standard deviation for each measurement. 
+*Uses descriptive activity names to name the activities in the data set
+*Appropriately labels the data set with descriptive variable names. 
+*From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+The final output dataset has one record per subject - activity combination and has the mean of a subset of the original variables (the mean and standard deviation variables).
+
+##Data Dictionary
+
+The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz. 
+
+Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag). 
+
+Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing fBodyAcc-XYZ, fBodyAccJerk-XYZ, fBodyGyro-XYZ, fBodyAccJerkMag, fBodyGyroMag, fBodyGyroJerkMag. (Note the 'f' to indicate frequency domain signals). 
---
+++ run_analysis.R
@@ -0,0 +1,61 @@
+#Written by Matt F. in Dec. 2015.
+#Implementation of Coursera Getting and Cleaning Data Course Project
+
+library(dplyr)
+
+setwd('C:/Users/matt/Dropbox/Documents/coursera/getting and cleaning data/Course Project')
+
+download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip','raw.zip')
+unzip('raw.zip')
+
+
+
+#### Step 1 ####
+#Desired output is dataframe containing all data (X,Y,and subject data for both test and train).
+
+cols <- read.table('UCI\ HAR\ Dataset/features.txt')
+
+train.subject <- read.table('UCI\ HAR\ Dataset/train/subject_train.txt',header=FALSE,col.names=c('subject'))
+train.x <- read.table('UCI\ HAR\ Dataset/train/X_train.txt',header=FALSE,col.names=cols[,2])
+train.y <- read.table('UCI\ HAR\ Dataset/train/Y_train.txt',header=FALSE,col.names=c('activity'))
+
+test.subject <- read.table('UCI\ HAR\ Dataset/test/subject_test.txt',header=FALSE,col.names=c('subject'))
+test.x <- read.table('UCI\ HAR\ Dataset/test/X_test.txt',header=FALSE,col.names=cols[,2])
+test.y <- read.table('UCI\ HAR\ Dataset/test/Y_test.txt',header=FALSE,col.names=c('activity'))
+
+
+all.data <- rbind(cbind(train.subject,train.x,train.y),cbind(test.subject,test.x,test.y))
+
+
+#### Step 2 ####
+#Desired output is dataframe containing subject and Y data as well as mean and std measurement column only.
+
+#Get the indices of the mean and standard deviation columns
+mean.or.std.indices <- grep('std|mean\\.',names(all.data))
+#Instructions aren't clear about whether meanFreq and angle(tBodyAccMean,gravity) should be included
+#I'm going with the most restrictive definition here. 66 columns that are defined as mean and std in the readme file.
+#Add subject and y variables to list of desired columns
+desired.indices <- append(mean.or.std.indices,c(1,ncol(all.data)))
+mean.or.std <- all.data[,desired.indices]
+
+#### Step 3 ####
+#Desired output is same as step 2 but with y column converted to descriptive labels
+activities <- read.table('UCI\ HAR\ Dataset/activity_labels.txt',header=FALSE,col.names=c('activity','activity_desc'))
+out3 <- merge(mean.or.std,activities)
+
+#### Step 4 ####
+#Desired output is same as step 3 but with column names more easily understood but someone less familiar with this study
+out4 <- out3
+cols <- names(out4)
+cols <- gsub('\\.','',cols)
+cols <- gsub('^f','Fourier',cols)
+cols <- gsub('^t','Trans',cols)
+cols <- gsub('mean','Mean',cols)
+cols <- gsub('std','Std',cols)
+names(out4) <- cols
+
+#### Step 5 ####
+#Desired output is dataframe containing mean of each measurement column in step4 for each subject, activity combination
+all(complete.cases(out4)) #No missing values
+out5 <- out4 %>% group_by(subject,activity,activity_desc) %>% summarise_each(funs(mean))
+write.table(out5,'table5.txt',row.name=FALSE)
